12a8c128d40d3da27b4fc61687756067e3898658
ofega <fegaeze@gmail.com>
2019-10-01 06:45:53 +0100

    Added local storage

--- src/App.js
+++ src/App.js
@@ -1,33 +1,78 @@
 import React from 'react';
 import uuid from 'uuid';
 
+import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
+
 
 class App extends React.Component {
-  
+
   constructor() {
-    super()
+    super();
 
     this.state = {
-      todos: [
-        {
-          task: 'Organize Garage',
-          id: 1528817077286,
-          completed: false
-        },
-        {
-          task: 'Bake Cookies',
-          id: 1528817084358,
-          completed: false
-        }
-      ]
+      todos: JSON.parse(localStorage.getItem('todos')) || [],
+      newTask: ''
     }
   }
 
+  handleInputChange = (e) => {
+    this.setState({
+      ...this.state,
+      newTask: e.target.value
+    })
+  }
+
+  markCompleted = (e) => {
+    e.persist();
+    
+    this.setState(prevState => ({
+      todos: prevState.todos.map(item => {
+        if (item.id !== e.target.id) {
+          console.log(item);
+          return item;
+        }
+
+        console.log(item);
+        return { id: item.id, task: item.task, completed: true };
+      })
+    }))
+  }
+
+  addTodo = (e) => {
+    e.preventDefault();
+
+    this.setState(prevState => ({
+      todos: [...prevState.todos, {
+        task: this.state.newTask,
+        id: uuid(),
+        completed: false
+      }],
+      newTask: ''
+    }), () => {
+      localStorage.setItem('todos', JSON.stringify(this.state.todos));
+    })
+  }
+
+  clearCompleted = () => {
+
+  }
 
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        <TodoForm 
+          newTask={this.state.newTask} 
+          addTodo={this.addTodo} 
+          clearCompleted={this.clearCompleted} 
+          handleInputChange={this.handleInputChange}
+        />
+
+        <TodoList 
+          todos={this.state.todos} 
+          markCompleted={this.markCompleted} 
+        />
       </div>
     );
   }
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+export default class TodoForm extends Component {
+    render() {
+        return (
+            <form>
+                <input
+                    type="text" 
+                    placeholder="Enter your todo here" 
+                    name="todo" 
+                    value={this.props.newTask} 
+                    onChange={this.props.handleInputChange}
+                />
+
+                <div>
+                    <button onClick={this.props.addTodo}>Add Todo</button>
+                    <button onClick={this.props.clearCompleted}>Clear Completed</button>
+                </div>
+            </form>
+        )
+    }
+}
